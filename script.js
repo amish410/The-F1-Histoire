a8dde56c1e380a68de7b266d094a42967e716fa4
Amish410 <135924285+Amish410@users.noreply.github.com>
2025-07-24 20:21:26 -0500

    championship data

--- script.js
+++ script.js
@@ -14,21 +14,45 @@ fetch('drivers.json')
     })
 
 function showdrivers() {
-    let year = document.getElementById("drivers").value;
-    if (year < 1950 || year > 2020) {
-        console.log("Please input a valid number within the range :)")
+    let year = parseInt(document.getElementById("drivers").value);
+    if (isNaN(year) || year < 1950 || year > 2020) {
         document.getElementById("drivererror").style.display = "inline";
         return;
     } else {
         document.getElementById("drivererror").style.display = "none";
     }
+
+    let length = drivers[year].length;
+    var table = document.getElementById("driversstandings");
+
+    while (table.rows.length > 1) {
+      table.deleteRow(1);
+    }
+
+    for (let i = 0; i < length; i++) {
+      let row = table.insertRow(i+1);
+      for (let i = 0; i < 6; i++) {
+        row.insertCell(i)
+      }
+      let td = row.getElementsByTagName("td")
+      let position = drivers[year][i].position;
+      let nation = drivers[year][i].nation;
+      let driver = drivers[year][i].driver;
+      let team = drivers[year][i].team;
+      let points = drivers[year][i].points;
+
+      td[0].innerHTML = position;
+      td[1].innerHTML = year
+      td[2].innerHTML = nation
+      td[3].innerHTML = driver
+      td[4].innerHTML = team
+      td[5].innerHTML = points
+    }
 }
 
 function showconstructors() {
-    const year = parseInt(document.getElementById("constructors").value);
-
+    let year = parseInt(document.getElementById("constructors").value);
     if (isNaN(year) || year < 1958 || year > 2020) {
-        console.log("Please input a valid number within the range :)");
         document.getElementById("constructorerror").style.display = "inline";
         return;
     } else {
@@ -47,12 +71,10 @@ function showconstructors() {
       for (let i = 0; i < 4; i++) {
         row.insertCell(i);
       }
-      var td = row.getElementsByTagName("td");
+      let td = row.getElementsByTagName("td");
       let position = constructors[year][i].position;
       let team = constructors[year][i].team;
       let points = constructors[year][i].points;
-
-      console.log("Inserting:", position, team, points);
 
       td[0].innerHTML = position;
       td[1].innerHTML = year;
